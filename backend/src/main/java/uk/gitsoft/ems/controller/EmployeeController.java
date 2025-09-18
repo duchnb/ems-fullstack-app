4892c804f3e14833c30b55349365fadc012fd2a6
Bogdan <bduchnowski@gmail.com>
2025-09-18 19:13:21 +0800

    feat: Add create and fetch Employee by ID functionality

--- backend/src/main/java/uk/gitsoft/ems/controller/EmployeeController.java
+++ backend/src/main/java/uk/gitsoft/ems/controller/EmployeeController.java
@@ -1,8 +1,13 @@
 package uk.gitsoft.ems.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gitsoft.ems.dto.EmployeeDto;
 import uk.gitsoft.ems.service.EmployeeService;
 
 @AllArgsConstructor
@@ -13,5 +18,9 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     //build add Employee REST API
-
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+       EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+       return new ResponseEntity<>(savedEmployee,HttpStatus.CREATED);
+    }
 }
--- backend/src/main/java/uk/gitsoft/ems/service/impl/EmployeeServiceImpl.java
+++ backend/src/main/java/uk/gitsoft/ems/service/impl/EmployeeServiceImpl.java
@@ -4,21 +4,31 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gitsoft.ems.dto.EmployeeDto;
 import uk.gitsoft.ems.entity.Employee;
+import uk.gitsoft.ems.exception.ResourceNotFoundException;
 import uk.gitsoft.ems.mapper.EmployeeMapper;
 import uk.gitsoft.ems.repository.EmployeeRepository;
 import uk.gitsoft.ems.service.EmployeeService;
 
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
     private final EmployeeRepository employeeRepository;
-    private EmployeeService employeeService;
 
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
-       Employee savedEmployee = employeeRepository.save(employee);
+        Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
 }
